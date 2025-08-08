3c5ecd7f9749347a74a331131d321271a9dfce40
jessecrans <jesse.crans@live.nl>
2025-08-08 11:11:45 +0200

    fixed unused import

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import blogPosts from "../data/blogPosts.json";
 import { getContrastTextColor, getTagColor } from "../util";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = () => {
   return (
